f2bc7d126ccaee8215529533973cfd4c16cd252b
Jordi Puiggené Valls <jordi.puiggene@gmail.com>
2013-04-04 17:52:39 +0200

    Fixed bikalabs/Bika-LIMS#678 List Patient samples

--- bika/health/browser/patient/samples.py
+++ bika/health/browser/patient/samples.py
@@ -1,8 +1,21 @@
 from bika.lims.browser.sample import SamplesView
-from bika.health.permissions import *
+from Products.CMFCore.utils import getToolByName
 
 
 class SamplesView(SamplesView):
+
     def __init__(self, context, request):
         super(SamplesView, self).__init__(context, request)
-        self.contentFilter['getPatientUID'] = self.context.UID()
+
+    def contentsMethod(self, contentFilter):
+        tool = getToolByName(self.context, self.catalog)
+        state = [x for x in self.review_states if x['id'] == self.review_state][0]
+        for k, v in state['contentFilter'].items():
+            self.contentFilter[k] = v
+        tool_samples = tool(contentFilter)
+        samples = {}
+        for sample in (p.getObject() for p in tool_samples):
+            for ar in sample.getAnalysisRequests():
+                if ar['Patient'] == self.context.UID():
+                    samples[sample.getId()] = sample
+        return samples.values()
