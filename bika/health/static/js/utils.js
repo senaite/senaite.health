4b66885c15370f33fa1542fa2b60159d0c8487f7
Jordi Puiggené Valls <jordi.puiggene@gmail.com>
2013-06-15 01:20:30 +0200

    Fixed bikalabs/Bika-LIMS#928 Small. Hazardous icon

--- bika/health/static/js/utils.js
+++ bika/health/static/js/utils.js
@@ -29,6 +29,14 @@ $(document).ready(function(){
 		}
 	}
 	
+	/* #HACK
+	 * https://github.com/bikalabs/Bika-LIMS/issues/928
+	 * Tricky and foolish stuff to override the hazardous icon in health.
+	 * Seems that image resources override doesn't work in overrides.zcml
+	 * (see bika/health/overrides.zcml and bika/health/static/overrides.zcml)
+	 */
+	$("img[src$='bika.lims.images/hazardous.png']").attr('src', window.portal_url + "/++resource++bika.health.images/hazardous.png");
+	$("img[src$='bika.lims.images/hazardous_big.png']").attr('src', window.portal_url + "/++resource++bika.health.images/hazardous_big.png");
 });
 }(jQuery));
 
