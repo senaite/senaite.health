75a4a58424b717e462dbd5d93ba62f795c1708f8
Jordi Puiggené Valls <jordi.puiggene@gmail.com>
2013-08-26 16:13:26 +0200

    Refactoring of AR results report

--- bika/health/browser/analysisrequest/publish.py
+++ bika/health/browser/analysisrequest/publish.py
@@ -1,9 +1,17 @@
 from bika.lims.browser.publish import doPublish
 from bika.health import bikaMessageFactory as _h
 from bika.lims import bikaMessageFactory as _
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class AnalysisRequestPublish(doPublish):
+
+    template = ViewPageTemplateFile("report_results.pt")
+
+    def __call__(self):
+        self.ar = self.analysis_requests[0]
+        self.patient = self.ar.Schema()['Patient'].get(self.ar)
+        return super(AnalysisRequestPublish, self).__call__()
 
     def get_mail_subject(self, ar):
         subject, totline = doPublish.get_mail_subject(self, ar)
