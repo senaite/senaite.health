c427ccbf56d9b1e6ecc147c1efa06ba036be03f9
Jordi Puiggené Valls <jordi.puiggene@gmail.com>
2013-08-28 00:20:52 +0200

    Fix bikalabs/Bika-LIMS#1029 When creating an AR for a Case, CPID on the AR should default to that of the Case

--- bika/health/browser/batch/getbatchinfo.py
+++ bika/health/browser/batch/getbatchinfo.py
@@ -25,19 +25,15 @@ class ajaxGetBatchInfo(BrowserView):
             value = patient.getPatientIdentifiersStr()
             patientids = len(value) > 0 and "("+value+")" or ''
 
-        ret = {'Client': client and "<a class='edit_client' href='%s/base_edit'>%s</a>"%(client.absolute_url(), client.Title()) or '',
-               'ClientID': client and client.getClientID() or '',
+        ret = {'ClientID': client and client.getClientID() or '',
                'ClientUID': client and client.UID() or '',
                'ClientTitle': client and client.Title() or '',
-               'Patient': patient and "<a class='edit_patient' href='%s/edit'>%s</a> %s"%(patient.absolute_url(), patient.Title(), patientids) or '',
                'PatientID': patient and patient.getPatientID() or '',
                'PatientUID': patient and patient.UID() or '',
                'PatientTitle': patient and patient.Title() or '',
-               'Doctor': doctor and "<a class='edit_doctor' href='%s/edit'>%s</a>"%(doctor.absolute_url(), doctor.Title()) or '',
                'DoctorID': doctor and doctor.getDoctorID(),
                'DoctorUID': doctor and doctor.UID() or '',
-               'DoctorTitle': doctor and doctor.Title() or '',
-               }
+               'DoctorTitle': doctor and doctor.Title() or ''}
 
         return json.dumps(ret)
 
