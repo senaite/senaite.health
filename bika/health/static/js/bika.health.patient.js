64ee42b6194fa67597a969c4c8e53b91fdf3607b
Jordi Puiggené <jordi.puiggene@gmail.com>
2015-01-14 20:35:27 +0100

    Custom overlay handler for patients

--- bika/health/static/js/bika.health.patient.js
+++ bika/health/static/js/bika.health.patient.js
@@ -437,13 +437,36 @@ function HealthPatientGlobalWidgetEditView() {
     }
 }
 
-
+/**
+ * Patient's overlay edit view Handler. Used by add buttons to
+ * manage the behaviour of the overlay.
+ */
 function HealthPatientOverlayHandler() {
     var that = this;
-    that.load = function() {
-    }
 
-    that.onLoad = function(overlay) {
-        console.warn("Hola caracola");
+    // Needed for bika.lims.loader to register the object at runtime
+    that.load = function() {}
+
+    /**
+     * Event fired on overlay.onLoad()
+     * Hides undesired contents from inside the overlay and also
+     * loads additional javascripts still not managed by the bika.lims
+     * loader
+     */
+    that.onLoad = function(event) {
+
+        // Manually remove remarks
+        event.getOverlay().find("#archetypes-fieldname-Remarks").remove();
+
+        // Remove menstrual status widget to avoid my suicide
+        // with a "500 service internal error"
+        event.getOverlay().find("#archetypes-fieldname-MenstrualStatus").remove();
+
+        // Address widget
+        $.ajax({
+            url: 'bika_widgets/addresswidget.js',
+            dataType: 'script',
+            async: false
+        });
     }
 }
