2784033203a6868b686a3ab7bbfd824c78364c47
Pau Soliva <psoliva@naralabs.com>
2015-02-06 14:43:00 +0100

    Content type created.

--- bika/health/upgrade/to3007.py
+++ bika/health/upgrade/to3007.py
@@ -5,31 +5,20 @@ from bika.health.permissions import AddInsuranceCompany
 from zExceptions import BadRequest
 
 def upgrade(tool):
-    """ Add InsuranceCompany content type.
+    """ Create InsuranceCompany content type.
     """
     # Add insurance company in bika_setup_catalog
     portal = aq_parent(aq_inner(tool))
     at = getToolByName(portal, 'archetype_tool')
     at.setCatalogsByType('InsuranceCompany', ['bika_setup_catalog'])
-
+    # Re-run changed conf files.
     setup = portal.portal_setup
-    setup.runImportStepFromProfile('profile-bika.lims:default', 'controlpanel')
-
+    setup.runImportStepFromProfile('profile-bika.health:default', 'typeinfo')
+    setup.runImportStepFromProfile('profile-bika.health:default', 'controlpanel')
+    setup.runImportStepFromProfile('profile-bika.health:default', 'factorytool')
+    setup.runImportStepFromProfile('profile-bika.health:default', 'propertiestool')
+    # Define permissions
     mp = portal.manage_permission
     mp(AddInsuranceCompany, ['Manager', 'Owner', 'LabManager', 'LabClerk'], 1)
-
-    try:
-        import pdb;pdb.set_trace()
-        #bika_setup = portal._getOb('bika_setup')
-        #bsc = getToolByName(portal, 'bika_setup_catalog', None)
-        #bsc.reindexObject()
-        #portal.insurancecompany.reindexObjects()
-        #obj = bika_setup._getOb('insurancecompany')
-        #obj.unmarkCreationFlag()
-        #obj.reindexObject()
-
-    except BadRequest:
-        # folder already exists
-        pass
 
     return True
