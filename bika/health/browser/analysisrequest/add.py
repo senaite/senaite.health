381d5b5315b2e8fd60f54e8391dbc5ba2c81096c
Pau Soliva <psoliva@naralabs.com>
2015-03-02 18:05:36 +0100

    class override

---
+++ bika/health/browser/analysisrequest/add.py
@@ -0,0 +1,21 @@
+from bika.lims.browser.analysisrequest.add import AnalysisRequestAddView as AnalysisRequestAddViewLIMS
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
+    """
+    The main AR Add form adapted for health usage
+    """
+    health_template = ViewPageTemplateFile("templates/ar_add.pt")
+
+    def __call__(self):
+        # Getting the checkbox value
+        enable_bika_request_field = self.context.bika_setup.Schema().getField('EnableBikaAnalysisRequestRequestForm')
+        enable_bika_request = enable_bika_request_field.get(self.context.bika_setup)
+        if enable_bika_request:
+            # Use the template defined on BikaLIMS
+            return AnalysisRequestAddViewLIMS.__call__(self)
+        else:
+            # Use the Health's template
+            self.request.set('disable_border', 1)
+            return self.health_template()
