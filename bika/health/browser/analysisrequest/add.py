9c8835fb5d5c4fd3d1dcdac422c33f1e98023032
Pau Soliva <psoliva@naralabs.com>
2015-03-13 13:03:12 +0100

    Changing my mind, maybe it's better a simple template.

--- bika/health/browser/analysisrequest/add.py
+++ bika/health/browser/analysisrequest/add.py
@@ -1,6 +1,7 @@
 from bika.lims.browser.analysisrequest.add import AnalysisRequestAddView as AnalysisRequestAddViewLIMS
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
+from Products.CMFPlone.utils import _createObjectByType
+from Products.CMFCore.utils import getToolByName
 
 class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
     """
@@ -8,6 +9,12 @@ class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
     """
     health_template = ViewPageTemplateFile("templates/ar_add.pt")
     patient_template = ViewPageTemplateFile("templates/ar_addpatient.pt")
+
+    def __init__(self, context, request):
+        AnalysisRequestAddViewLIMS.__init__(self, context, request)
+        # An array where are located all the the schema patient's fields to show on the ar_add
+        self._pfields = ['PatientID', 'Surname', 'Firstname', 'BirthDate', 'Gender', 'HomePhone', 'MobilePhone',
+                         'BusinessPhone', 'EmailAddress']
 
     def __call__(self):
         # Getting the checkbox value
@@ -20,3 +27,30 @@ class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
             # Use the Health's template
             self.request.set('disable_border', 1)
             return self.health_template()
+
+    def get_patient_fields(self):
+        """
+        Get only the pateint's fields filtered by 'visibility' state
+        :param visibility: The state which the fields have to be shown
+        :return: a list with the filtered fields
+        """
+        patients_brains = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patient')
+        # Only one brain is needed
+        schema_fields = patients_brains[0].getObject().Schema().fields()
+        #cont = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patients')
+        #schema_fields = cont.portal_url.getPortalObject()["Patient"]
+
+        fields = []
+        for field in schema_fields:
+            if field.required or field.getName() in self._pfields:
+                fields.append(field)
+        import pdb; pdb.set_trace()
+        return fields
+
+    def get_patient_context(self):
+        """
+        :return: The patient context
+        """
+        patients_brains = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patient')
+        return patients_brains[0].getObject()
+
