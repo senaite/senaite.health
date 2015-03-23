e3653df19f722c6f260ef00060aca903adc5f360
Pau Soliva <psoliva@naralabs.com>
2015-03-23 18:18:30 +0100

    ArAdd's html almost done. Dynamic content remaining and filter by client.

--- bika/health/browser/analysisrequest/add.py
+++ bika/health/browser/analysisrequest/add.py
@@ -1,6 +1,5 @@
 from bika.lims.browser.analysisrequest.add import AnalysisRequestAddView as AnalysisRequestAddViewLIMS
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFPlone.utils import _createObjectByType
 import json
 from Products.CMFCore.utils import getToolByName
 from bika.lims.browser.widgets import AddressWidget
@@ -15,12 +14,14 @@ class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
     doctor_referrer_template = ViewPageTemplateFile("templates/ar_add_doctor_referrer.pt")
     insurance_template = ViewPageTemplateFile("templates/ar_insurance.pt")
     analyses_template = ViewPageTemplateFile("templates/ar_analyses.pt")
+    prices_template = ViewPageTemplateFile("templates/ar_prices.pt")
 
     def __init__(self, context, request):
         AnalysisRequestAddViewLIMS.__init__(self, context, request)
         # An array where are located all the the schema patient's fields to show on the ar_add
         self._pfields = ['PatientID', 'Surname', 'Firstname', 'BirthDate', 'Gender', 'HomePhone', 'MobilePhone',
                          'BusinessPhone', 'EmailAddress']
+        self.col_count = 1
         self.w = AddressWidget()
 
     def __call__(self):
@@ -35,31 +36,6 @@ class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
             self.request.set('disable_border', 1)
             return self.health_template()
 
-    def get_patient_fields(self):
-        """
-        Get only the pateint's fields filtered by 'visibility' state
-        :param visibility: The state which the fields have to be shown
-        :return: a list with the filtered fields
-        """
-        patients_brains = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patient')
-        # Only one brain is needed
-        schema_fields = patients_brains[0].getObject().Schema().fields()
-        #cont = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patients')
-        #schema_fields = cont.portal_url.getPortalObject()["Patient"]
-
-        fields = []
-        for field in schema_fields:
-            if field.required or field.getName() in self._pfields:
-                fields.append(field)
-        return fields
-
-    def get_patient_context(self):
-        """
-        :return: The patient context
-        """
-        patients_brains = getToolByName(self.context, 'bika_patient_catalog')(portal_type='Patient')
-        return patients_brains[0].getObject()
-
     def get_json_format(self, d):
         """
         Given some data, it get its json format.
@@ -67,3 +43,25 @@ class AnalysisRequestAddView(AnalysisRequestAddViewLIMS):
         :return: The formatted data in JSON.
         """
         return json.dumps(d)
+
+    def getDepartments(self):
+        """
+        It obtain the different departments
+        :return: A list with the department names and UID's [(UID,Name),....]
+        """
+        dep = getToolByName(self.context, 'bika_setup_catalog')(portal_type='Department')
+        l = []
+        for i in dep:
+            l.append((i.UID, i.Title))
+        return l
+
+    def categoryInDepartment(self, category, department):
+        """
+        Check if the category belongs to the department
+        :param category: The category UID
+        :param department: The department UID
+        :return: True or False
+        """
+        return getToolByName(self.context, 'bika_setup_catalog')(UID=category)[0]\
+            .getObject().getDepartment().UID() == department
+
