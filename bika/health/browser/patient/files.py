54fe4079ab5d600338be52634dda98873b2dc511
Pau Soliva <psoliva@naralabs.com>
2015-06-04 10:36:17 +0200

    HEALTH-258 Add "File attachment" on Patient

---
+++ bika/health/browser/patient/files.py
@@ -0,0 +1,16 @@
+from bika.lims import bikaMessageFactory as _
+from plone.app.content.browser.interfaces import IFolderContentsView
+from plone.app.layout.globals.interfaces import IViewView
+from zope.interface import implements
+from bika.lims.browser.multifile import MultifileView
+
+
+class PatientMultifileView(MultifileView):
+    implements(IFolderContentsView, IViewView)
+
+    def __init__(self, context, request):
+        super(PatientMultifileView, self).__init__(context, request)
+        self.title = self.context.translate(_("Patient Attachments"))
+        self.context_actions = {_('Add'):
+                                {'url': 'createObject?type_name=Multifile',
+                                 'icon': '++resource++bika.lims.images/add.png'}}
