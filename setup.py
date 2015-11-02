167dd49e57db1290823f4b2b1d2470cbd8fef720
Pau Soliva <psoliva@naralabs.com>
2015-11-02 18:26:45 +0100

    New versions accepted

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='bika.health',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'bika.lims==3.1.9',
+          'bika.lims<3.2',
           'archetypes.schemaextender',
           'collective.wtf',
       ],
