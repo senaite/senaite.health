d9c93f1a61bb91d9d80a1a79d3288a486616a0c5
Espurna <psoliva@naralabs.com>
2017-05-26 14:39:02 +0200

    upgradestep

---
+++ bika/health/upgrade/v3_2_0_1705.py
@@ -0,0 +1,54 @@
+# This file is part of Bika Health
+#
+# Copyright 2011-2017 by its authors.
+# Some rights reserved. See LICENSE.txt, AUTHORS.txt.
+from Products.CMFCore.utils import getToolByName
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from bika.health import logger
+from bika.lims.upgrade import upgradestep
+from bika.lims.upgrade.utils import UpgradeUtils
+from bika.lims.upgrade.utils import migrate_to_blob
+
+product = 'bika.health'
+version = '3.2.0.1705'
+
+
+@upgradestep(product, version)
+def upgrade(tool):
+    portal = aq_parent(aq_inner(tool))
+    ut = UpgradeUtils(portal)
+    ufrom = ut.getInstalledVersion(product)
+    if ut.isOlderVersion(product, version):
+        logger.info("Skipping upgrade of {0}: {1} > {2}".format(
+                    product, ufrom, version))
+        # The currently installed version is more recent than the target
+        # version of this upgradestep
+        return True
+
+    logger.info("Upgrading {0}: {1} -> {2}".format(product, ufrom, version))
+    migareteFileFields(portal)
+
+    logger.info("{0} upgraded to version {1}".format(product, version))
+    return True
+
+
+def migareteFileFields(portal):
+    """
+    This function walks over all attachment types and migrates their FileField
+    fields.
+    """
+    portal_types = [
+        "Treatment"]
+    for portal_type in portal_types:
+        logger.info(
+            "Starting migration of FileField fields from {}."
+            .format(portal_type))
+        # Do the migration
+        migrate_to_blob(
+            portal,
+            portal_type=portal_type,
+            remove_old_value=True)
+        logger.info(
+            "Finished migration of FileField fields from {}."
+            .format(portal_type))
