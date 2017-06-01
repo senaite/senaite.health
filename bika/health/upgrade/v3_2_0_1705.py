a0a1173ef035857cf4fa0ef99c54e73ce9c97148
Campbell <campbell@bikalabs.com>
2017-06-01 11:38:22 +0200

    Fix health upgrade 1705: Do not overwrite indexes from bika.lims!

--- bika/health/upgrade/v3_2_0_1705.py
+++ bika/health/upgrade/v3_2_0_1705.py
@@ -2,10 +2,17 @@
 #
 # Copyright 2011-2017 by its authors.
 # Some rights reserved. See LICENSE.txt, AUTHORS.txt.
-from Products.CMFCore.utils import getToolByName
 from Acquisition import aq_inner
 from Acquisition import aq_parent
+
+from Products.CMFCore.utils import getToolByName
 from bika.health import logger
+from bika.health.catalog \
+    import getCatalogDefinitions as getCatalogDefinitionsHealth, \
+    getCatalogExtensions
+from bika.lims.catalog \
+    import getCatalogDefinitions as getCatalogDefinitionsLIMS
+from bika.lims.catalog import setup_catalogs
 from bika.lims.upgrade import upgradestep
 from bika.lims.upgrade.utils import UpgradeUtils
 from bika.lims.upgrade.utils import migrate_to_blob
@@ -21,13 +28,28 @@ def upgrade(tool):
     ufrom = ut.getInstalledVersion(product)
     if ut.isOlderVersion(product, version):
         logger.info("Skipping upgrade of {0}: {1} > {2}".format(
-                    product, ufrom, version))
+            product, ufrom, version))
         # The currently installed version is more recent than the target
         # version of this upgradestep
         return True
 
     logger.info("Upgrading {0}: {1} -> {2}".format(product, ufrom, version))
     migrateFileFields(portal)
+
+    setup = portal.portal_setup
+    setup.runImportStepFromProfile('profile-bika.lims:default', 'toolset')
+
+    # Adding colums and indexes if needed
+    pc = getToolByName(portal, 'portal_catalog')
+    # Updating lims catalogs if there is any change in them
+    logger.info("Updating catalogs if needed...")
+    catalog_definitions_lims_health = getCatalogDefinitionsLIMS()
+    catalog_definitions_lims_health.update(getCatalogDefinitionsHealth())
+    # Updating health catalogs if there is any change in them
+    setup_catalogs(
+        portal, catalog_definitions_lims_health,
+        catalogs_extension=getCatalogExtensions())
+    logger.info("Catalogs updated")
 
     logger.info("{0} upgraded to version {1}".format(product, version))
     return True
@@ -43,7 +65,7 @@ def migrateFileFields(portal):
     for portal_type in portal_types:
         logger.info(
             "Starting migration of FileField fields from {}."
-            .format(portal_type))
+                .format(portal_type))
         # Do the migration
         migrate_to_blob(
             portal,
@@ -51,4 +73,4 @@ def migrateFileFields(portal):
             remove_old_value=True)
         logger.info(
             "Finished migration of FileField fields from {}."
-            .format(portal_type))
+                .format(portal_type))
