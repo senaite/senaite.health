1874ecbf4f17ef385a97120b96f22da6ea25fbef
Nihadness <1992.nihad@gmail.com>
2017-11-06 13:58:24 +0100

    AR list view of Doctor filters properly now.

--- bika/health/browser/doctor/analysisrequests.py
+++ bika/health/browser/doctor/analysisrequests.py
@@ -4,14 +4,4 @@ from bika.health.browser.analysisrequests.view import AnalysisRequestsView as Ba
 class AnalysisRequestsView(BaseView):
     def __init__(self, context, request):
         super(AnalysisRequestsView, self).__init__(context, request)
-        self.contentFilter['DoctorUID'] = self.context.UID()
-
-    def isItemAllowed(self, obj):
-        """
-        Checks the BikaLIMS conditions and also checks filter bar conditions
-        @Obj: it is an analysis request brain.
-        @return: boolean
-        """
-        if not super(AnalysisRequestsView, self).isItemAllowed(obj):
-            return False
-        return self.context.UID() == obj.getDoctorUID
+        self.contentFilter['getDoctorUID'] = self.context.UID()
