29ca633057f1588534ebf04c1f513795a0367079
Jordi Puiggené <jpuiggene@naralabs.com>
2017-11-10 16:25:43 +0100

    Update setup.py and version

--- setup.py
+++ setup.py
@@ -1,53 +1,43 @@
 from setuptools import setup, find_packages
-import os
 
-version = '3.2.0'
+version = '3.2.0.1711'
 
 setup(name='bika.health',
       version=version,
-      description="Bika Health Open Source LIS",
-      long_description=open("README.md").read() + "\n\n" +
-                       open("docs/CHANGELOG.txt").read(),
+      description="Bika Health Evo",
+      long_description=open("README.rst").read() + "\n" +
+                       open("CHANGES.rst").read()+ "\n",
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Healthcare Industry",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        ],
-      keywords=['lims', 'health', 'bika', 'opensource'],
-      author='Bika Laboratory Systems',
-      author_email='support@bikalabs.com',
-      maintainer='Naralabs',
-      maintainer_email='info@naralabs.com',
-      url='http://github.com/bikalabs/bika.health',
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Zope2",
+          "Intended Audience :: Healthcare Industry",
+          "Intended Audience :: Information Technology",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+          "Programming Language :: Python",
+
+      ],
+      keywords=['lims', 'bika', 'opensource'],
+      author='Naralabs',
+      author_email='support@naralabs.com',
+      url='https://github.com/naralabs/bika.lims',
       license='AGPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['bika'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'bika.lims==1.0.0',
+          'bika.lims',
           'archetypes.schemaextender',
           'collective.wtf',
       ],
-      extras_require={
-          'test': [
-                  'plone.app.testing',
-                  'robotsuite',
-                  'robotframework-selenium2library',
-                  'plone.app.robotframework',
-                  'robotframework-debuglibrary',
-              ]
-      },
       entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+          # -*- Entry points: -*-
+          [z3c.autoinclude.plugin]
+          target = plone
+          """,
+)
