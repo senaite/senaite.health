7869c7b22e8e8d183ef25bebe03c7ede23a211ef
Jordi Puiggené <jpuiggene@naralabs.com>
2018-01-15 20:28:50 +0100

    Clean imports and PEP8

--- bika/health/browser/patients/folder_view.py
+++ bika/health/browser/patients/folder_view.py
@@ -7,24 +7,16 @@
 
 import types
 
-from Products.ATContentTypes.content import schemata
-from Products.Archetypes import atapi
 from Products.CMFCore.utils import getToolByName
-from bika.lims import bikaMessageFactory as _b, api
-from bika.lims.content.contact import Contact
-from bika.lims.interfaces import IClient, ILabContact
 from bika.health import bikaMessageFactory as _
 from bika.health.catalog import CATALOG_PATIENT_LISTING
-from bika.lims.browser.bika_listing import BikaListingView
-from bika.lims.utils import logged_in_client
-from bika.health.config import PROJECTNAME
-from bika.health.interfaces import IPatients
 from bika.health.permissions import *
+from bika.lims import bikaMessageFactory as _b, api
+from bika.lims.browser.bika_listing import BikaListingView
+from bika.lims.interfaces import IClient, ILabContact
 from plone.app.content.browser.interfaces import IFolderContentsView
-from plone.app.folder.folder import ATFolder, ATFolderSchema
 from plone.app.layout.globals.interfaces import IViewView
 from zope.interface.declarations import implements
-import json
 
 # Available Columns of the Patients View Table (see: `get_columns`)
 COLUMNS = ['getPatientID',
@@ -49,7 +41,8 @@ class PatientsView(BikaListingView):
                               'sort_order': 'reverse'}
         self.context_actions = {}
         self.title = self.context.translate(_("Patients"))
-        self.icon = self.portal_url + "/++resource++bika.health.images/patient_big.png"
+        self.icon = '{}/++resource++bika.health.images/patient_big.png'.format(
+            self.portal_url)
         self.show_sort_column = False
         self.show_select_row = False
         self.show_select_column = False
@@ -132,9 +125,11 @@ class PatientsView(BikaListingView):
         client_uid = api.get_uid(client)
         self.contentFilter['getPrimaryReferrerUID'] = client_uid
 
-    def get_columns(self, sieve=[]):
+    def get_columns(self, sieve=None):
         """Get Table Columns and filter out keys listed in sieve
         """
+        if not sieve:
+            sieve = list()
         if type(sieve) not in (types.ListType, types.TupleType):
             raise RuntimeError("Sieve must be a list type")
         # filter out columns listed in sieve
@@ -164,22 +159,23 @@ class PatientsView(BikaListingView):
                     'icon': '++resource++bika.lims.images/add.png'
                 }
             else:
-                msg = _("Cannot create patients without any system clients configured.")
+                msg = _("Cannot create patients without any system clients "
+                        "configured.")
                 addPortalMessage(self.context.translate(msg))
 
         if mtool.checkPermission(ViewPatients, self.context):
-            self.review_states[0]['transitions'].append({'id':'deactivate'})
+            self.review_states[0]['transitions'].append({'id': 'deactivate'})
             self.review_states.append(
                 {'id': 'inactive',
                  'title': _b('Dormant'),
                  'contentFilter': {'inactive_state': 'inactive'},
-                 'transitions': [{'id':'activate'}, ],
+                 'transitions': [{'id': 'activate'}, ],
                  'columns': self.get_columns()})
             self.review_states.append(
                 {'id': 'all',
                  'title': _b('All'),
-                 'contentFilter':{},
-                 'transitions':[{'id':'empty'}, ],
+                 'contentFilter': {},
+                 'transitions': [{'id': 'empty'}, ],
                  'columns': self.get_columns()})
             stat = self.request.get("%s_review_state" % self.form_id, 'default')
             self.show_select_column = stat != 'all'
@@ -194,19 +190,20 @@ class PatientsView(BikaListingView):
         """
         if 'obj' not in item:
             return None
-        # TODO: Remember to use https://github.com/bikalabs/bika.lims/pull/1846/files#diff-40dfcc4bde98b3ea8f3d9f985776675aR51
-        # Note: attr and replace_url
+
         item['getPatientID'] = obj.getPatientID
         item['getGender'] = obj.getGender
         item['getAgeSplittedStr'] = obj.getAgeSplittedStr
         item['getBirthDate'] = self.ulocalized_time(obj.getBirthDate)
         item['getClientPatientID'] = obj.getClientPatientID
-        item['replace']['getPatientID'] = "<a href='%s/analysisrequests'>%s</a>" % \
-            (item['url'], item['getPatientID'])
+        item['replace'][
+            'getPatientID'] = "<a href='%s/analysisrequests'>%s</a>" % \
+                              (item['url'], item['getPatientID'])
         item['replace']['getClientPatientID'] = "<a href='%s'>%s</a>" % \
-            (item['url'], item['getClientPatientID'])
+                                                (item['url'],
+                                                 item['getClientPatientID'])
         item['replace']['Title'] = "<a href='%s/analysisrequests'>%s</a>" % \
-            (item['url'], item['Title'])
+                                   (item['url'], item['Title'])
         prTitle = obj.getPrimaryReferrerTitle
         prURL = obj.getPrimaryReferrerURL
         item['getPrimaryReferrerTitle'] = prTitle
