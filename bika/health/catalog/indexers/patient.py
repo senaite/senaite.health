98d12bdf87520fb7601bca2e35621ce806e317d3
Nihad M <1992.nihad@gmail.com>
2018-05-01 08:27:00 +0200

    'listing_searchable_text' index optimizations. (#76)
    * Use 'None' as default value while getting an attribute
    
    * Do not override 'listing_searchable_text' of AR's from senaite.core
    
    * Always try to get attributes from the brain.
    
    * Retrieve the object brain only once in listing searchable indexer.
    
    * Review Changes.

--- bika/health/catalog/indexers/patient.py
+++ bika/health/catalog/indexers/patient.py
@@ -19,17 +19,12 @@ def listing_searchable_text(instance):
     entries = []
     catalog = api.get_tool(CATALOG_PATIENT_LISTING)
     columns = catalog.schema()
-
+    brains = catalog({"UID": api.get_uid(instance)})
+    brain = brains[0] if brains else None
     for column in columns:
-        try:
-            value = api.safe_getattr(instance, column)
-        except:
-            logger.debug("{} has no attribute called '{}' ".format(
-                            repr(instance), column))
-            continue
-        if not value:
-            continue
-        parsed = api.to_searchable_text_metadata(value)
+        brain_value = api.safe_getattr(brain, column, None)
+        instance_value = api.safe_getattr(instance, column, None)
+        parsed = api.to_searchable_text_metadata(brain_value or instance_value)
         entries.append(parsed)
 
     # Concatenate all strings to one text blob
