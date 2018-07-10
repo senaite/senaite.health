b9ffc975effd3baec11d6864b090e2306217e767
Espurna <psoliva@naralabs.com>
2018-07-10 18:03:06 +0200

    fixing import error

--- bika/health/content/doctors.py
+++ bika/health/content/doctors.py
@@ -5,22 +5,17 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from Products.ATContentTypes.content import schemata
+import json
+
 from Products.Archetypes import atapi
-from Products.CMFCore import permissions
-from Products.CMFCore.utils import getToolByName
-from Products.Five.browser import BrowserView
-from bika.lims.browser.bika_listing import BikaListingView
 from Products.Archetypes.utils import DisplayList
-from bika.health.config import PROJECTNAME
-from bika.health.interfaces import IDoctors
-from plone.app.layout.globals.interfaces import IViewView
-from bika.lims import bikaMessageFactory as _b
-from bika.health import bikaMessageFactory as _
-from bika.health.permissions import *
+from Products.CMFCore.utils import getToolByName
 from plone.app.folder.folder import ATFolder, ATFolderSchema
 from zope.interface.declarations import implements
-import json
+
+from bika.health.config import PROJECTNAME
+from bika.health.interfaces import IDoctors
+from bika.lims.utils import isActive
 
 schema = ATFolderSchema.copy()
 
