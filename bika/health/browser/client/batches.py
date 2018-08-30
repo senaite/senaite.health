4abbb9be7dfa4c4edc500f9014368cb053f22213
Jordi Puiggené <jpuiggene@naralabs.com>
2018-08-30 12:01:54 +0200

    Add button was not displayed in Client's Cases view

--- bika/health/browser/client/batches.py
+++ bika/health/browser/client/batches.py
@@ -6,17 +6,17 @@
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
 from Products.CMFCore.utils import getToolByName
+from bika.health import bikaMessageFactory as _
 from bika.lims.browser.client import ClientBatchesView as BaseClientBatchesView
 from bika.health.browser.batch.batchfolder import BatchFolderContentsView as HealthBatchesView
 
 
 class BatchesView(BaseClientBatchesView, HealthBatchesView):
 
-    def contentsMethod(self, contentFilter):
-        batches = {}
-        bc = getToolByName(self.context, "bika_catalog")
-        for batch in bc(portal_type='Batch',
-                        getClientUID = self.context.UID()):
-            batch = batch.getObject()
-            batches[batch.UID()] = batch 
-        return batches.values()
+    def __call__(self):
+        url = self.portal.absolute_url()
+        self.context_actions[_('Add')] = dict(
+            url="{}{}".format(url, "/batches/createObject?type_name=Batch"),
+            icon="{}{}".format(url, "/++resource++bika.lims.images/add.png"))
+
+        return HealthBatchesView.__call__(self)
