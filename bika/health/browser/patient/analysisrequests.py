a5bb270d85bb3f43c021e982976eb9ad7af46bca
Jordi Puiggené <jp@naralabs.com>
2018-10-30 22:10:13 +0100

    Insufficient privileges when "AR add" is clicked inside Patient (#112)

--- bika/health/browser/patient/analysisrequests.py
+++ bika/health/browser/patient/analysisrequests.py
@@ -5,50 +5,31 @@
 # Copyright 2018 by it's authors.
 # Some rights reserved. See LICENSE.rst, CONTRIBUTORS.rst.
 
-from Products.CMFCore.utils import getToolByName
-from bika.lims.browser.analysisrequest import AnalysisRequestWorkflowAction,\
-    AnalysisRequestsView
-from bika.lims import bikaMessageFactory as _b
 from bika.health import bikaMessageFactory as _
-from bika.lims.permissions import *
-from bika.health.permissions import *
-from bika.lims.subscribers import doActionFor, skip
-from bika.lims.utils import isActive
-from operator import itemgetter
-from plone.app.content.browser.interfaces import IFolderContentsView
-from plone.app.layout.globals.interfaces import IViewView
-from zope.i18n import translate
-from zope.interface import implements
-import json
-import plone
+from bika.lims import api
+from bika.lims.browser import BrowserView
+from bika.lims.browser.analysisrequest import AnalysisRequestsView
 
 
 class AnalysisRequestsView(AnalysisRequestsView):
 
     def __init__(self, context, request):
         super(AnalysisRequestsView, self).__init__(context, request)
-        # Filter by patient
         self.contentFilter['getPatientUID'] = self.context.UID()
         self.show_all = True
         self.columns['BatchID']['title'] = _('Case ID')
 
-    def __call__(self):
-        self.context_actions = {}
-        wf = getToolByName(self.context, 'portal_workflow')
-        mtool = getToolByName(self.context, 'portal_membership')
-        addPortalMessage = self.context.plone_utils.addPortalMessage
-        PR = self.context.getPrimaryReferrer()
-        if isActive(self.context):
-            if mtool.checkPermission(AddAnalysisRequest, PR):
-                #client contact required
-                contacts = [c for c in PR.objectValues('Contact') if
-                            wf.getInfoFor(c, 'inactive_state', '') == 'active']
-                if contacts:
-                    self.context_actions[self.context.translate(_('Add'))] = {
-                        'url': PR.absolute_url() + "/ar_add",
-                        'icon': '++resource++bika.lims.images/add.png'}
-                else:
-                    msg = _("Client contact required before request may be submitted")
-                    addPortalMessage(self.context.translate(msg))
 
-        return super(AnalysisRequestsView, self).__call__()
+class AnalysisRequestAddRedirectView(BrowserView):
+    """Artifact to redirect the user to AR Add view when 'AR Add' button is
+    clicked in Patient's Analysis Requests view
+    """
+
+    def __call__(self):
+        client = self.context.getPrimaryReferrer()
+        url = "{}/{}".format(api.get_url(client), "ar_add")
+        qs = self.request.getHeader("query_string")
+        if qs:
+            url = "{}?{}".format(url, qs)
+        self.request.response.redirect(url)
+        return
