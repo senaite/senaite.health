448cbdf46bad116bd4cc7eb26786ade3c4209152
Ramon Bartl <rb@ridingbytes.com>
2020-06-24 21:45:57 +0200

    Added upgrade step for senaite.health

--- src/senaite/health/upgrade/v02_00_000.py
+++ src/senaite/health/upgrade/v02_00_000.py
@@ -1,18 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from bika.health.config import PROJECTNAME
+from bika.lims import api
 from senaite.core.upgrade import upgradestep
 from senaite.core.upgrade.utils import UpgradeUtils
 from senaite.health import logger
-from senaite.health.config import PROJECTNAME
 
 version = "2.0.0"
 profile = "profile-{0}:default".format(PROJECTNAME)
+
+INSTALL_PRODUCTS = [
+    "senaite.health",
+]
 
 
 @upgradestep(PROJECTNAME, version)
 def upgrade(tool):
     portal = tool.aq_inner.aq_parent
-    setup = portal.portal_setup
+    setup = portal.portal_setup  # noqa
     ut = UpgradeUtils(portal)
     ver_from = ut.getInstalledVersion(PROJECTNAME)
 
@@ -26,5 +31,22 @@ def upgrade(tool):
 
     # -------- ADD YOUR STUFF BELOW --------
 
+    # Install the new SENAITE HEALTH package
+    install_senaite_health(portal)
+
     logger.info("{0} upgraded to version {1}".format(PROJECTNAME, version))
     return True
+
+
+def install_senaite_health(portal):
+    """Install new SENAITE HEALTH Add-on
+    """
+    logger.info("Installing SENAITE HEALTH 2.x...")
+    qi = api.get_tool("portal_quickinstaller")
+
+    for p in INSTALL_PRODUCTS:
+        if qi.isProductInstalled(p):
+            continue
+        logger.info("Installing '{}' ...".format(p))
+        qi.installProduct(p)
+    logger.info("Installing SENAITE HEALTH 2.x [DONE]")
